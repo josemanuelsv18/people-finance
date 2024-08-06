1deef9f7fabb5a68baccb52b2cb2835d10847166
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2024-08-05 23:14:20 -0500

    database table sql complete

--- database/database_pf.sql
+++ database/database_pf.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE people_finance;
 
+USE people_finance;
+
 CREATE TABLE users(
 	id_user INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(50) NOT NULL,
@@ -16,14 +18,45 @@ CREATE TABLE accounts(
 	type ENUM('Savings', 'Checking') NOT NULL,
     balance DECIMAL(15,2) NOT NULL,
     id_user INT,
-    FOREIGN KEY (id_users) REFERENCES users(id_user)
+    FOREIGN KEY (id_user) REFERENCES users(id_user)
 );
 
 CREATE TABLE transactions(
 	id_transaction INT PRIMARY KEY AUTO_INCREMENT,
     type ENUM('Income', 'Spending')NOT NULL,
-    amount DECIMAL(15,2),
+    amount DECIMAL(15,2) NOT NULL,
     tran_date DATETIME NOT NULL,
     description VARCHAR(255),
     id_account INT,
+    id_category INT,
+    FOREIGN KEY (id_account) REFERENCES accounts(id_account),
+    FOREIGN KEY (id_category) REFERENCES category(id_category)
+);
+
+CREATE TABLE category(
+	id_category INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE budget(
+	id_budget INT PRIMARY KEY,
+    limit_amount DECIMAL(15,2) NOT NULL,
+    init_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    id_user INT,
+    id_category INT,
+    FOREIGN KEY (id_user) REFERENCES users(id_user),
+    FOREIGN KEY (id_category) REFERENCES category(id_category)
+);
+
+CREATE TABLE saving_goal(
+	id_saving INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    amount_goal DECIMAL(15,2) NOT NULL,
+    amount_save DECIMAL(15,2) NOT NULL,
+    init_date DATE NOT NULL,
+    goal_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    id_user INT,
+    FOREIGN KEY (id_user) REFERENCES users(id_user)
 );

b4ded4935227bb65e6c203092fe10f7731f9345c
Josemanuel Sifontes <josemanuelsv18@gmail.com>
2024-08-12 19:02:03 -0500

    database connection whith app user established

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,9 +1,26 @@
 import mysql.connector
+import xml_reader
 
 class DbConnector:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.my_db = ''
+    
+    def get_my_db(self):
+        return self.my_db
 
+    def connection_sql(self):
+        obj_xmlreader = xml_reader.XmlReader('archives/connection.xml')
+        obj_xmlreader.data_xml()
+        print(type(obj_xmlreader.get_host()))
+        self.my_db = mysql.connector.connect(
+            host=obj_xmlreader.get_host(),
+            user=obj_xmlreader.get_username(),
+            password=obj_xmlreader.get_password()
+        )
+        print(self.my_db)
+
+obj_prueba = DbConnector()
+obj_prueba.connection_sql()
     
 
 #mydb = mysql.connector.connect(
